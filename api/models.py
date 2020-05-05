91cf98ff113e179477cf5052f0529a1bf536360f
Nelson <nhiguera@vigigo.com>
2020-05-05 01:04:46 -0400

    cambios al modelo

--- api/models.py
+++ api/models.py
@@ -3,11 +3,12 @@ from django.db import models
 class Ingrediente(models.Model):
     nombre = models.TextField()
     descripcion = models.TextField()
+class IngredienteEnHamburguesa(models.Model):
+    path = models.TextField()
 
 class Hamburguesa(models.Model):
     nombre = models.TextField()
     descripcion = models.TextField()
     precio = models.IntegerField()
     imagen = models.TextField()
-    ingredientes = models.ManyToManyField(Ingrediente)
-
+    ingredientes = models.ManyToManyField(IngredienteEnHamburguesa)
