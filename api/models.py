a26ebcc9a9357fe0809c5b427a22dd7a03820dc6
Nelson <nhiguera@vigigo.com>
2020-05-05 00:14:06 -0400

    Eliminado campo id de los modelos

--- api/models.py
+++ api/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 class Ingrediente(models.Model):
-    id = models.AutoField(primary_key=True)
     nombre = models.TextField()
     descripcion = models.TextField()
 
 class Hamburguesa(models.Model):
-    id = models.AutoField(primary_key=True)
     nombre = models.TextField()
     descripcion = models.TextField()
     precio = models.IntegerField()
