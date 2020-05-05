9c40fb7d8be93ebc7f64555f42625adbd5e3425f
njgarrido <37156093+njgarrido@users.noreply.github.com>
2020-05-05 04:21:51 -0400

    path

--- api/urls.py
+++ api/urls.py
@@ -15,5 +15,5 @@ router.register(r'ingrediente', views.IngredienteViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
